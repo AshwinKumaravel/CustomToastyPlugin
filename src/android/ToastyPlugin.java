9ad5ea2d1eae04088647520e4e4906e3a06509d4
Ashwin <ashwin.kumaravel@cesltd.com>
2020-03-03 21:51:02 +0530

    Added scanner service  for android

---
+++ src/android/ToastyPlugin.java
@@ -0,0 +1,97 @@
+package com.stanleyidesis.cordova.plugin;
+// The native Toast API
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.scandit.datacapture.barcode.capture.BarcodeCapture;
+import com.scandit.datacapture.barcode.capture.BarcodeCaptureListener;
+import com.scandit.datacapture.barcode.capture.BarcodeCaptureSession;
+import com.scandit.datacapture.barcode.capture.BarcodeCaptureSettings;
+import com.scandit.datacapture.barcode.capture.SymbologySettings;
+import com.scandit.datacapture.barcode.data.Barcode;
+import com.scandit.datacapture.barcode.data.Symbology;
+import com.scandit.datacapture.barcode.data.SymbologyDescription;
+import com.scandit.datacapture.barcode.ui.overlay.BarcodeCaptureOverlay;
+import com.scandit.datacapture.core.capture.DataCaptureContext;
+import com.scandit.datacapture.core.data.FrameData;
+import com.scandit.datacapture.core.source.Camera;
+import com.scandit.datacapture.core.source.FrameSourceState;
+import com.scandit.datacapture.core.ui.DataCaptureView;
+import com.scandit.datacapture.core.ui.viewfinder.RectangularViewfinder;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class ToastyPlugin extends CordovaPlugin {
+
+
+    @Override
+  public boolean execute(String action, JSONArray args,
+    final CallbackContext callbackContext) {
+      // Verify that the user sent a 'show' action
+      if (!action.equals("show")) {
+        callbackContext.error("\"" + action + "\" is not a recognized action.");
+        return false;
+      }
+      String message;
+      String duration;
+      try {
+        JSONObject options = args.getJSONObject(0);
+        message = options.getString("message");
+        duration = options.getString("duration");
+      } catch (JSONException e) {
+        callbackContext.error("Error encountered: " + e.getMessage());
+        return false;
+      }
+        Toast toast = Toast.makeText(cordova.getActivity(), "Hai", Toast.LENGTH_LONG);
+        // Display toast
+        toast.show();
+
+//        Context context = cordova.getActivity().getApplicationContext();
+//        this.openNewActivity(context);
+
+      // Send a positive result to the callbackContext
+      PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+      callbackContext.sendPluginResult(pluginResult);
+      return true;
+
+  }
+
+//    private void openNewActivity(Context context) {
+//        Intent intent = new Intent(context, NewActivity.class);
+//        this.cordova.getActivity().startActivity(intent);
+//    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
