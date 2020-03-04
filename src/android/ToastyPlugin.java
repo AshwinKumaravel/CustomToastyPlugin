124ca100707e4f80d118c75e56b89bd7b9c35615
Ashwin <ashwin.kumaravel@cesltd.com>
2020-03-04 12:42:09 +0530

    no message

--- src/android/ToastyPlugin.java
+++ src/android/ToastyPlugin.java
@@ -16,32 +16,12 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
-
-import com.scandit.datacapture.barcode.capture.BarcodeCapture;
-import com.scandit.datacapture.barcode.capture.BarcodeCaptureListener;
-import com.scandit.datacapture.barcode.capture.BarcodeCaptureSession;
-import com.scandit.datacapture.barcode.capture.BarcodeCaptureSettings;
-import com.scandit.datacapture.barcode.capture.SymbologySettings;
-import com.scandit.datacapture.barcode.data.Barcode;
-import com.scandit.datacapture.barcode.data.Symbology;
-import com.scandit.datacapture.barcode.data.SymbologyDescription;
-import com.scandit.datacapture.barcode.ui.overlay.BarcodeCaptureOverlay;
-import com.scandit.datacapture.core.capture.DataCaptureContext;
-import com.scandit.datacapture.core.data.FrameData;
-import com.scandit.datacapture.core.source.Camera;
-import com.scandit.datacapture.core.source.FrameSourceState;
-import com.scandit.datacapture.core.ui.DataCaptureView;
-import com.scandit.datacapture.core.ui.viewfinder.RectangularViewfinder;
-
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.Arrays;
-import java.util.HashSet;
 
 public class ToastyPlugin extends CordovaPlugin {
 
@@ -68,8 +48,8 @@ public class ToastyPlugin extends CordovaPlugin {
         // Display toast
         toast.show();
 
-//        Context context = cordova.getActivity().getApplicationContext();
-//        this.openNewActivity(context);
+        Context context = cordova.getActivity().getApplicationContext();
+        this.openNewActivity(context);
 
       // Send a positive result to the callbackContext
       PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
@@ -78,10 +58,10 @@ public class ToastyPlugin extends CordovaPlugin {
 
   }
 
-//    private void openNewActivity(Context context) {
-//        Intent intent = new Intent(context, NewActivity.class);
-//        this.cordova.getActivity().startActivity(intent);
-//    }
+    private void openNewActivity(Context context) {
+        Intent intent = new Intent(context, NewActivity.class);
+        this.cordova.getActivity().startActivity(intent);
+    }
 }
 
 
