7878eb8206f60edf0074901e90ee8b5e5aa890e7
Ashwin <ashwin.kumaravel@cesltd.com>
2020-03-06 18:57:40 +0530

    Added iOS files with frame worksTesting

---
+++ src/ios/ToastyPlugin.h
@@ -0,0 +1,20 @@
+//
+//  ToastyPlugin.h
+//  ToastyPluginTest
+//
+//  Created by Ashwin on 05/03/20.
+//
+
+#import <Cordova/CDVViewController.h>
+#import <Cordova/CDVCommandDelegateImpl.h>
+#import <Cordova/CDVCommandQueue.h>
+
+@interface ToastyPlugin : CDVViewController
+
+@end
+
+@interface MainCommandDelegate : CDVCommandDelegateImpl
+@end
+
+@interface MainCommandQueue : CDVCommandQueue
+@end
