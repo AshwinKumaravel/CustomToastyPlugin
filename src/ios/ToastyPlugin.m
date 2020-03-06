65e69c7c9c103889de5c71b1c146684c16e9da3c
Ashwin <ashwin.kumaravel@cesltd.com>
2020-03-07 00:02:23 +0530

    Added iOS files with frame worksTesting

--- src/ios/ToastyPlugin.m
+++ src/ios/ToastyPlugin.m
@@ -1,13 +1,8 @@
-
 #import <Cordova/CDV.h>
 #import "ToastyPlugin.h"
 
 
-@interface ToastyPlugin ()
-
-
-@end
-
+static NSString *const _Nonnull licenseKey = @"AcwuTyW6OyCfAw/oXRAg/7w/zNFaMBqI/XXo8hZcKMWydVYgu0dYgktRqT72eZOoPX6vMTg7KxyjQRt78nZJXmJ7wf/sc2tvcmgdi+xugYNeL1NbHlbu5jhlSTEyR1jTb2M0f6BY26tVL4G1hzvXOdoxCg/iDwcrNt9dwzfiBs7HAR5mLKgxmsap6WBqG3OS/vob53Z5dr1dYtmCV4aKmuvU19tYB/9Hr4hFJi8lMji/0f/WJKaFlWcoqsFlQqD8gRONQNWcSiM4wUZiIQrz8Kz+dH1wGh43pHrqGJjklZfoEANoqEoNe+Oyyq9TVKDaRQAjFlZ4bLIBFIExqPn+xQAP3EDNeuYJdTcaOpllxA8QLHKb04tSZg2st0XEQ/WkbD551a8DwfeM4nJt5hHKcpTbREC9ZE4jCBZFndKMfOmQf/l7qhzU9nQSvHQcuYMGNBEVG5UMpCCyotp2m6lG9nSGP/yH5sz8DwMDlHSfbkdarrQKR01f1P4hbVnqIjN5QQ/v4Pvbz/Jj/0ae/aDIMng3aljRzxWWhLEoXEKiGU40LTR3mLR9EezkYYpC+/oOAbBwhjZudNwaUerRsM8h2ys03l0TvPZPvqDbC2njAIHSZxHF5AIKRzfPgzyrC1A0XA4aZ8bAFRQ68QEbEfF16qL6A76mpaZsGVN28cPOt1zEey9KzmiY8ZmJ9YQ+76YipnCVFz4zQcMohgwXhwGaUZQGQTo5ESUpwzodkAhkBG7gakf8D1XEr2SVNmDUERh9TSYrG7Z8TIWIWUToy8P8G6jM+50t+dKS+wnbcmzIwfFpuWI1XrQOd1oyQlV94LdChZjRN8iL";
 
 
 @implementation ToastyPlugin
@@ -17,6 +12,58 @@
     CDVPluginResult* pluginResult = nil;
     NSString* callbackId = command.callbackId;
     [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+}
+
+
+- (id)initWithNibName:(NSString*)nibNameOrNil bundle:(NSBundle*)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+
+    }
+    return self;
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+      
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark View lifecycle
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    // View defaults to full size.  If you want to customize the view's size, or its subviews (e.g. webView),
+    // you can do so here.
+
+    [super viewWillAppear:animated];
+
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+
 }
 
 @end
